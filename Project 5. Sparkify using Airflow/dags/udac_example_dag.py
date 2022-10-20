c5e898272e252840ab0c7ec220f8287d2803ee09
unknown <kevin.pereda26@gmail.com>
2022-10-20 12:36:32 -0500

    add files for staging operators

---
+++ Project 5. Sparkify using Airflow/dags/udac_example_dag.py
@@ -0,0 +1,83 @@
+from datetime import datetime, timedelta
+import os
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator, LoadFactOperator,
+                                LoadDimensionOperator, DataQualityOperator)
+from helpers import SqlQueries
+
+# AWS_KEY = os.environ.get('AWS_KEY')
+# AWS_SECRET = os.environ.get('AWS_SECRET')
+
+default_args = {
+    'owner': 'udacity',
+    'start_date': datetime(2019, 1, 12),
+    'depends_on_past': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_retry': True,
+}
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='0 * * * *',
+          catchup = False,
+        )
+
+start_operator = DummyOperator(task_id='Begin_execution',  dag=dag)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    dag=dag,
+    aws_connection_id = "aws_credentials",
+    redshift_conn_id = "redshift",
+    target_table="staging_events",
+    create_statement = SqlQueries.staging_events_table_create,
+    s3_bucket = "udacity-dend",
+    s3_key = "log_data",
+    s3_key_json_path = "log_json_path.json"
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    dag=dag,
+    aws_connection_id = "aws_credentials",
+    redshift_conn_id = "redshift",
+    target_table="staging_songs",
+    #create_statement = SqlQueries.staging_songs_table_create,
+    s3_bucket = "udacity-dend",
+    s3_key = "song_data"
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    dag=dag
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    dag=dag
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    dag=dag
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    dag=dag
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    dag=dag
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    dag=dag
+)
+
+end_operator = DummyOperator(task_id='Stop_execution',  dag=dag)
---
+++ Project 5. Sparkify using Airflow/plugins/operators/stage_redshift.py
@@ -0,0 +1,59 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class StageToRedshiftOperator(BaseOperator):
+    ui_color = '#358140'
+    template_fields = ("s3_key", "s3_key_json_path", )  # The json path could also be templated in case the data changes schema in long periods of time
+    copy_sql = """
+        COPY {}
+        FROM '{}'
+        ACCESS_KEY_ID '{}'
+        SECRET_ACCESS_KEY '{}'
+        JSON '{}'
+    """
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_conn_id='redshift',
+                 aws_credentials_id="aws_credentials",
+                 target_table="",
+                 create_statement="",
+                 s3_bucket="",
+                 s3_key="",
+                 s3_key_json_path=None,
+                 *args, **kwargs):
+        super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
+        self.redshift_conn_id = redshift_conn_id
+        self.aws_credentials_id = aws_credentials_id
+        self.target_table = target_table
+        self.create_statement = create_statement
+        self.s3_bucket = s3_bucket
+        self.s3_key = s3_key
+        self.s3_key_json_path = s3_key_json_path
+
+    def execute(self, context):
+        aws_hook = AwsHook(self.aws_credentials_id)
+        credentials = aws_hook.get_credentials()
+        redshift = PostgresHook(postgres_conn_id=self.redshift_conn_id)
+
+        self.log.info(f'Dropping table {self.target_table} in case exists')
+        redshift.run(f"DROP TABLE IF EXISTS {self.target_table}")
+
+        self.log.info('Creating the table with statement')
+        redshift.run(self.create_statement)
+
+        self.log.info('Copying data from S3 to Redshift')
+        rendered_key = self.s3_key.format(**context)
+        s3_path = f"s3://{self.s3_bucket}/{rendered_key}"
+        json_path = f"s3://{self.s3_bucket}/{self.s3_key_json_path.format(**context)}" if self.s3_key_json_path is not None else 'auto'
+
+        formatted_sql = StageToRedshiftOperator.copy_sql.format(
+            self.target_table,
+            s3_path,
+            credentials.access_key,
+            credentials.secret_key,
+            json_path
+        )
+
+        redshift.run(formatted_sql)
